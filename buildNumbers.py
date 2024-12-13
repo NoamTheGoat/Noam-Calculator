eb1580adaac351ef39d31a70710f305906fda1c9
NoamTheGoat <noamrose19@gmail.com>
2024-12-13 21:29:31 +0200

    takes the input from the users as a list that each element is 1 character and builds the numbers that are in independent character

---
+++ buildNumbers.py
@@ -0,0 +1,20 @@
+def buildNumbers(expression):
+    result = []
+
+    i=0
+    while i < len(expression):
+        c = expression[i]
+
+        if c.isdigit():
+            num = c
+            while i+1<len(expression) and expression[i + 1].isdigit(): #i + 1 < len(s) and
+                i += 1
+                num += expression[i]
+            result.append(num)
+
+        else:
+            result.append(c)
+
+        i+=1
+
+    return result
