6f2764576a0993bb96be2a813e5d20dbe53a4b18
NoamTheGoat <noamrose19@gmail.com>
2024-12-15 20:49:12 +0200

    changed logic so it will work, oprnd>0 is leading to an infinate loop, so i changed it to oprnd>=1

--- customArithmeticOperations/digSumArthOpr.py
+++ customArithmeticOperations/digSumArthOpr.py
@@ -2,7 +2,7 @@ def digSumArthOpr(oprnd):
     while not oprnd.is_integer():
         oprnd*=10
     sum=0
-    while oprnd>0:
+    while oprnd>=1:
         sum+=int(oprnd%10)
         oprnd/=10
     return sum
