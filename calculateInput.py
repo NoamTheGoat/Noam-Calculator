007cb2f65b8d9b18c5364669fe653e7d0d9bd16b
NoamTheGoat <noamrose19@gmail.com>
2024-12-17 15:40:44 +0200

    added exceptions checking for the input

--- calculateInput.py
+++ calculateInput.py
@@ -1,5 +1,9 @@
+from webbrowser import Error
+
 from buildNumbers import buildNumbers
+from exceptions import SyntaxInputError
 from infixToPostfix import infixToPostfix
+from exceptionHandling import *
 from minusAndTildaHandling import minusAndTildaHandling
 from evalPostfix import evalPostfix
 
@@ -12,17 +16,24 @@ def calculate(equastion):
     #otherErrorChecks
 
     #minusAndTildaErrorCheck(eq_List)
+    try:
+        legalChecking(eq_list)
 
-    eq_list = buildNumbers(eq_list)
 
-    eq_list =  minusAndTildaHandling(eq_list)
+        eq_list = buildNumbers(eq_list)
+
+        eq_list =  minusAndTildaHandling(eq_list)
 
     #infixToPostfixErrorCheck(eq_List)
 
-    eq_list = infixToPostfix(eq_list)
+        eq_list = infixToPostfix(eq_list)
 
-    solution = evalPostfix(eq_list)
+        solution = evalPostfix(eq_list)
+    except OverflowError:
+        print("The expression of the equation is to big to calculate")
+    except Exception as e:
+        print(e)
 
     return solution
 
-print(calculate("4--------4-4"))
+print(calculate("3++3"))
