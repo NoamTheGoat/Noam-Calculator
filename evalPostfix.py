02955956195370e323fec87457935bf39ff899af
NoamTheGoat <noamrose19@gmail.com>
2024-12-17 15:33:03 +0200

    now handles arithmetic exceptions such as deviding be zero

--- evalPostfix.py
+++ evalPostfix.py
@@ -40,8 +40,12 @@ def evalBin(operand2, operator, operand1):
     elif operator == '*':
         return operand1 * operand2
     elif operator == '/':
+        if(operand2==0):
+            raise ArithmeticError("Cannot devide by 0")
         return operand1 / operand2
     elif operator == '^':
+        if operand1<0 and operand2>-1 and operand2<1:
+            raise ArithmeticError("Cannot do squirt to negative number.")
         return operand1 ** operand2
     elif operator == '%':
         return operand1 % operand2
@@ -54,8 +58,16 @@ def evalBin(operand2, operator, operand1):
 
 def evalUn(operand, operator):
     if operator == '!':
+        if not operand == int(operand):
+            raise ArithmeticError("Cannot do a factorial to a non negative number.")
+        if operand < 0:
+            raise ArithmeticError("Cannot do a factorial to a negative number.")
+        if operand >170:
+            raise OverflowError()
         return factorialOpr(operand)
     elif operator == '#':
+        if operand < 0:
+            raise ArithmeticError("Cannot sum the digits of a negative number.")
         return digSumArthOpr(operand)
     elif operator == 'u':  # Unary minus
         return -operand
